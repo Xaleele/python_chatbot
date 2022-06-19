d9c3151438b4a1e331ba972cc6e1b131cc48e947
Alex <alexylee415@gmail.com>
2022-06-18 22:28:38 -0700

    Added class Chatbot. Included basic responses for rule-based.

--- chatbot.py
+++ chatbot.py
@@ -3,4 +3,14 @@ import random
 
 class ChatBot:
 
-    pass
+    negative_responses = ('no', 'nope', 'nah', 'naw', 'not a chance','sorry')
+
+    exit_commands = ('quit', 'pause', 'exit', 'goodbye', 'bye', 'later')
+
+    random_questions = (
+        "Why are you here?",
+        "Are there many people?",
+        "What is your favorite food?",
+        "Do you know where USA is?"
+        "What's your favorite GPU brand?"
+    )
